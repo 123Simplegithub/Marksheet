c28ade8b2c29da01d874fc8113efd7492ecd1fa6
M Farhan Hashmi <160288691+123Simplegithub@users.noreply.github.com>
2024-05-06 02:50:12 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,14 @@
+let maxmarks = 850
+let obtainedmarks = 640
+let percentage=(obtainedmarks*100) / maxmarks
+if (percentage>=90 && percentage <=100)
+    {
+      console.log("Your grade is A+")  
+    } else if(percentage >=80 && percentage<=90)
+       { console.log("Your grade is A")
+} else if (percentage >=70 && percentage<=80)
+    {
+        console.log("Your grade is B")
+    } else {
+        console.log("You are passed")
+    }
